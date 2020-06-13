467880b8860f83707b50dee58b258827c533bfe1
Brian <brianluiscoimbra@gmail.com>
2020-06-13 17:04:25 -0300

    Prato do dia adicionado na tela inicial e direcionamento para as outras páginas (lanches, pratos, sobremesas, bebidas) funcionando

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LanchesPage } from '../lanches/lanches';
+import { BebidasPage } from '../bebidas/bebidas';
+import { SobremesasPage } from '../sobremesas/sobremesas';
+import { PratosPage } from '../pratos/pratos';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +14,22 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+
+  abreLanches(){
+    this.navCtrl.push(LanchesPage, {}, {} );    
+  }
+
+  abrePratos(){
+    this.navCtrl.push(PratosPage, {}, {} );    
+  }
+
+  abreBebidas(){
+    this.navCtrl.push(BebidasPage, {}, {} );    
+  }
+
+  abreSobremesas(){
+    this.navCtrl.push(SobremesasPage, {}, {} );    
+  }
+  
 
 }
